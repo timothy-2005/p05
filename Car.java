cf7d8c1cd704e5bafe38767184ad3c35f1e1a9aa
timothy <tim2005129@icloud.com>
2024-03-10 19:15:02 -0500

    20240310

--- Car.java
+++ Car.java
@@ -1,3 +1,33 @@
-public class Car {
-    
+public class Car extends Toy{
+    private static int absoluteSpeed = 8;
+    private boolean isMovingRightward;
+    private int speed;
+
+    public Car(int x, int y) {
+        super("car.png", x, y);
+        isMovingRightward = true;
+        speed = absoluteSpeed;
+    }
+
+    public void draw(){
+        if (toySaga.isNightMode()){
+            move();
+            drawCarNightMode();
+        }else{
+            draw();
+        }
+    }
+    private void drawCarNightMode(){
+        toySaga.pushMatrix();
+        toySaga.rotate(0.0f);
+        toySaga.translate(x, y);
+        if (!isMovingRightward){
+            toySaga.scale(-1.0f, 1.0f);
+        }
+        toySaga.image(image, 0.0f, 0.0f);
+        toySaga.popMatrix();
+    }
+    public void flipMoveDirection(){
+        
+    }
 }
--- TeddyBear.java
+++ TeddyBear.java
@@ -1,3 +1,57 @@
-public class TeddyBear {
-    
+import processing.core.PApplet;
+
+public class TeddyBear extends Toy{
+    private Callout callout;
+    private float rotation;
+    private boolean rotationDirection;
+
+    public TeddyBear(int x, int y){
+        super("teddybear.png", x, y);
+        callout = new Callout(x, y);
+        rotation = 0;
+        rotationDirection = true;
+    }
+    public float getRotation(){
+        return rotation;
+    }
+    public void setRotation(float rotation){
+        this.rotation = rotation;
+    }
+    public void setRotationDirection(boolean direction){
+        rotationDirection = direction;
+    }
+    public boolean getRotationDirection(){
+        return rotationDirection;
+    }
+    public void draw(){
+        if (toySaga.isNightMode()){
+            drawTeddyBearAtNightMode();
+        }else{
+            super.draw();
+        }
+    }
+    public void drawTeddyBearAtNightMode(){
+        move();
+        toySaga.pushMatrix();
+        toySaga.translate(x, y);
+        toySaga.rotate(rotation * PApplet.PI / 2);
+        if (toySaga.getMode() == "NIGHT"){
+            toySaga.image(callout.image, 20f, -90f);
+        }
+        toySaga.image(image, 0.0f, 0.0f);
+        toySaga.popMatrix();
+    }
+    public void move(){
+        if (!toySaga.isNightMode()){
+            super.move();
+        }else{
+            rotation = getRotation() + PApplet.radians(3);
+            if (rotation >= 30){
+                setRotationDirection(false);
+            }else if(rotation <= -30){
+                setRotationDirection(true);
+            }
+            setRotation(rotation);
+        }
+    }
 }
